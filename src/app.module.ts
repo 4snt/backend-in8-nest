6f2dce09d89206f8e98b0c838bdd9a3c225fdbf2
4snt <murilo.escobedo@protonmail.com>
2025-06-14 09:27:34 -0300

    fix(appmodules): inclusão do Payment no appmodules

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { AuthModule } from './auth/auth.module';
 import { CheckoutModule } from './checkout/checkout.module';
 import { ImageModule } from './image/image.module';
 import { OrdersModule } from './orders/orders.module';
+import { PaymentModule } from './payment/payment.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
 import { ProductsModule } from './products/products.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    PaymentModule,
     ImageModule,
     AuthModule,
     ProductsModule,
